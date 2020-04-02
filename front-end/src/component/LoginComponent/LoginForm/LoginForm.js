81c7b4a3c2d31d76815cc6c9d0c8c7c1d41d5051
Nhu Nguyen <nhunguyen1259@gmail.com>
2020-04-02 21:42:26 +0700

    sửa form login

--- front-end/src/component/LoginComponent/LoginForm/LoginForm.js
+++ front-end/src/component/LoginComponent/LoginForm/LoginForm.js
@@ -8,61 +8,112 @@ import {
     Box,
     CssBaseline,
     Container,
+    Grid,
+    FormControlLabel,
+    Checkbox,
+    Link,
+    Paper,
     Typography
 } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 import {FormGroup} from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     margin: {
-        margin: theme.spacing(1),
+        margin: theme.spacing(0),
     },
-    extendedIcon: {
-        marginRight: theme.spacing(1),
+    // extendedIcon: {
+    //     marginRight: theme.spacing(1),
+    // },
+    paper: {
+        marginTop: theme.spacing(0),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+      },
+    submit: {
+        margin: theme.spacing(0),
+      },  
+    paperContainer: {
+        backgroundColor: '#333',
     },
 }));
+
 const LoginForm = props => {
     const classes= useStyles();
     return(
         <React.Fragment>
             <CssBaseline />
-            <Container maxWidth="xs" xs={6}>
+            <Container maxWidth="xs" xs={2}>
                 <Typography component="div" variant="body1">
-                    <Box bgcolor="success.main">
+                    <Box bgcolor="white">
                         <form onSubmit={props.onSubmit}>
-                            <h1>ABCDEF</h1>
-                            <InputLabel htmlFor="userName">
-                                User Name
-                            </InputLabel >
-                            <Input type="text"
-                                id="userName"
-                                placeholder="User Name"
-                                name="userName"
-                                value={props.value.userName}
-                                onChange={event => props.onChange(event, 'userName')} />
-                            <InputLabel htmlFor="passWord">
-                                Pass Word
-                            </InputLabel>
-                            <Input type="password"
-                                id="passWord"
-                                placeholder="Pass Word"
-                                name="passWord"
-                                value={props.value.passWord}
-                                onChange={event => props.onChange(event, 'passWord')} />
-                            <br />
-                            <Button
-                                variant="contained"
-                                type="submit"
-                                className={classes.margin}
-                                color="primary">
-                                Login
-                             </Button>
-                            <Button
-                                variant="contained"
-                                type="submit"
-                                className={classes.margin}
-                                color="primary">
-                                Clear
-                            </Button>
+                            <div className={classes.paper}>
+                                <h1>Login Form</h1>
+                                <form className={classes.form} noValidate>
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="userName"
+                                        label="Username"
+                                        name="userName"
+                                        autoComplete="username"
+                                        value={props.value.userName}
+                                        onChange={event => props.onChange(event, 'userName')}
+                                        autoFocus
+                                    />
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        value={props.value.passWord}
+                                        onChange={event => props.onChange(event, 'passWord')}
+                                        autoComplete="current-password"
+                                    />
+                                    <FormControlLabel
+                                        control={<Checkbox value="remember" color="primary" />}
+                                        label="Remember me"
+                                    />
+                                    <Button
+                                        type="submit"
+                                        variant="contained"
+                                        fullWidth
+                                        color="primary"
+                                        className={classes.submit}>
+                                        Sign In
+                                    </Button>
+                                    {/* <Button
+                                        variant="contained"
+                                        type="submit"
+                                        className={classes.margin}
+                                        color="primary">
+                                        Clear
+                                    </Button> */}
+                                    <Grid container>
+                                        <Grid item xs>
+                                        <Link href="#" variant="body2">
+                                            Forgot password?
+                                        </Link>
+                                        </Grid>
+                                        <Grid item>
+                                        <Link href="#" variant="body2">
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </div>                            
                         </form>
                     </Box>
                 </Typography>
